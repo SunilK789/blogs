1141ba2570a8f6d9d975261e8ce3b21a4c239086
Sunil Kumar <sunilpalarya@gmail.com>
2023-06-05 16:46:05 +0530

    update codes

--- query/index.js
+++ query/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
-app.get('/posts',(req, res)=>{})
+app.get('/posts',(req, res)=>{
+    res.send(posts);
+})
 
 app.post('/events',(req, res)=>{
     const {type, data} = req.body;
